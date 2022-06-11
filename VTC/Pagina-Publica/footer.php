52791ef9f98f2cceaa9c0e083af827cc06c41cff
Axel Mercado <39321438+Ammercado@users.noreply.github.com>
2022-06-11 16:39:16 -0300

    Delete VTC/Pagina-Publica directory

--- VTC/Pagina-Publica/footer.php
+++
@@ -1,3 +0,0 @@
-<?php
- echo "Copyright &copy;" . date("Y") .  " All rights reserved | Designed ". "<i class='fa fa-heart-o' aria-hidden='true'></i>" . " by " . "<a href='https://axtindev.tk' target='_blank'>". " AxTin " ."</a>"
- ?>
